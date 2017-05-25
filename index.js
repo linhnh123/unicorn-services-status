49251fab38585687f28ed1c407f762db0b9fad38
linhnh <hanlinhqn@gmail.com>
2017-05-25 16:10:12 +0700

    update api

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ if (Meteor.isServer) {
   Api.addRoute('status', {
     post: function() {
       const public = Meteor.settings.public;
-      const data = {
-        name: public && public.name ? public.name : Meteor.absoluteUrl(),
-        url: Meteor.absoluteUrl(),
-      };
-      return { status: 'OK', data: data };
+      return { status: 'OK', name: public && public.name ? public.name : 'UnSet' };
     },
   });
 }
