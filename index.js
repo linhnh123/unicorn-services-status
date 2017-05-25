c3f267f9a43dc691cbe155d00b1d778d04eca378
linhnh <hanlinhqn@gmail.com>
2017-05-25 15:33:22 +0700

    update api

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 if (Meteor.isServer) {
-  // Check ServicesStatus collection is exiting or not
-  try {
-    const exist = ServicesStatus.findOne({});
-  } catch (e) {
-    ServicesStatus = new Mongo.Collection('servicesStatus');
-  }
   var Api = new Restivus({
     useDefaultAuth: true,
     prettyJson: true
@@ -15,21 +9,8 @@ if (Meteor.isServer) {
       const data = {
         name: public && public.name ? public.name : Meteor.absoluteUrl(),
         url: Meteor.absoluteUrl(),
-        type: 'HEALTH_CHECK',
-        lastOnline: new Date(),
       };
-      try {
-        // Clear collection if have greater than 100 records
-        if (ServicesStatus.find({}).count() > 100) {
-          ServicesStatus.remove({});
-        }
-        // Insert new data
-        const id = ServicesStatus.insert(data);
-        return { status: 'OK', id: id };
-      } catch (e) {
-        const lastItem = ServicesStatus.findOne({}, {sort: {lastOnline: -1}, fields: {_id: 1}});
-        return lastItem ? { status: 'FAILED', id: lastItem._id } : { status: 'FAILED', data: data };
-      }
+      return { status: 'OK', data: data };
     },
   });
 }
